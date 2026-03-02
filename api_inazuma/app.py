47f84ff8d1d7ae9d491841ec106403ea167b50ab
Nickxx-22 <nicolas.bermudez.flores@alumnojoyfe.iepgroup.es>
2026-03-02 17:45:03 +0100

    update inazuma project

--- api_inazuma/app.py
+++ api_inazuma/app.py
@@ -111,7 +111,7 @@ def iniciar_sesion():
         return jsonify({"message": "⚠️ Se requiere nombre de usuario y contraseña"}), 400
 
     usuario = usuarios.find_one({"username": username})
-    if usuario and check_password_hash(usuario['password'], password):
+    if usuario and usuario['password'] == password:
         payload = {
             "sub": str(usuario["_id"]),
             "exp": datetime.utcnow() + timedelta(hours=1)
