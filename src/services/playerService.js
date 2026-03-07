346d93f5e296beb0304eb1b75f2b57c313b8b95b
Nickxx-22 <nicolas.bermudez.flores@alumnojoyfe.iepgroup.es>
2026-03-07 01:51:06 +0100

    update inazuma project

--- src/services/playerService.js
+++ src/services/playerService.js
@@ -1,5 +1,12 @@
 export async function getAllPlayers() {
-  const response = await fetch('http://127.0.0.1:5000/jugadores');
+  // Cambiamos la URL local por la de Render
+  const response = await fetch('https://api-inazuma.onrender.com/jugadores');
+  
   if (!response.ok) throw new Error('Error al cargar jugadores');
-  return await response.json(); // Esto devuelve la lista completa
-}
+  
+  const data = await response.json();
+  
+  // Aquí es donde verás el campo "matchStats" que configuramos
+  // y podrás aplicar los modificadores (0.5 para heredero y 0.3 para copia)
+  return data; 
+};
